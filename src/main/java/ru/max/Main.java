c49f0b075fefa3ccbebb59f39352807b87645383
Maxim Serov <maxim58r@gmail.com>
2020-07-20 18:35:22 +0300

    Initial commit

---
+++ src/main/java/ru/max/Main.java
@@ -0,0 +1,13 @@
+package ru.max;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("ru.max\\context.xml");
+
+        MessageRenderer renderer = (MessageRenderer) context.getBean("renderer");
+        renderer.printMessage();
+    }
+}
